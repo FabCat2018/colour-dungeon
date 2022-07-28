ff9d49594e213f7e0a9ebb8d7ff7dc6bb5c99e4d
Fabio Cataleta <fabio.cataleta@caplin.com>
2022-07-28 15:33:31 +0100

    Added multiple Switch components and styled into 3x3 grid

--- src/components/SwitchGrid.js
+++ src/components/SwitchGrid.js
@@ -1,11 +1,45 @@
+import "./SwitchGrid.css";
+
 import React from "react";
 import Switch from "./Switch";
 
 function SwitchGrid() {
   return (
-    <div>
-      <Switch />
-    </div>
+    <table className="button-grid">
+      <tr>
+        <td className="button-cell">
+          <Switch />
+        </td>
+        <td className="button-cell">
+          <Switch />
+        </td>
+        <td className="button-cell">
+          <Switch />
+        </td>
+      </tr>
+      <tr>
+      <td className="button-cell">
+          <Switch />
+        </td>
+        <td className="button-cell">
+          <Switch />
+        </td>
+        <td className="button-cell">
+          <Switch />
+        </td>
+      </tr>
+      <tr>
+      <td className="button-cell">
+          <Switch />
+        </td>
+        <td className="button-cell">
+          <Switch />
+        </td>
+        <td className="button-cell">
+          <Switch />
+        </td>
+      </tr>
+    </table>
   );
 }
 
