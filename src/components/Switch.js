494b07dd1c896af1bbca328794d27f3b7b0c0128
Fabio Cataleta <fabio.cataleta@caplin.com>
2022-07-29 15:08:03 +0100

    Implemented Switch affecting adjacent Switches logic

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -2,14 +2,33 @@ import "./Switch.css";
 
 function Switch({ rowIndex, cellIndex, isOn, allButtons, updateButton }) {
   const handleUpdate = () => {
-    const newGrid = allButtons.map((buttonRow, rowNumber) => {
-      return buttonRow.map((buttonCell, cellNumber) => {
-        if (rowNumber === rowIndex && cellNumber === cellIndex) {
-          return !buttonCell;
+    const newGrid = [...Array(allButtons.length)].map(() =>
+      Array(allButtons[0].length).fill(undefined)
+    );
+
+    for (let i = 0; i < newGrid.length; i++) {
+      for (let j = 0; j < newGrid[0].length; j++) {
+        if (rowIndex === i && cellIndex === j) {
+          newGrid[i][j] = !allButtons[i][j];
+
+          if (i - 1 > -1) {
+            newGrid[i - 1][j] = !allButtons[i - 1][j];
+          }
+          debugger;
+          if (i + 1 < allButtons.length) {
+            newGrid[i + 1][j] = !allButtons[i + 1][j];
+          }
+          if (j - 1 > -1) {
+            newGrid[i][j - 1] = !allButtons[i][j - 1];
+          }
+          if (j + 1 < allButtons[0].length) {
+            newGrid[i][j + 1] = !allButtons[i][j + 1];
+          }
+        } else if (newGrid[i][j] === undefined) {
+          newGrid[i][j] = allButtons[i][j];
         }
-        return buttonCell;
-      });
-    });
+      }
+    }
 
     updateButton(newGrid);
   };
